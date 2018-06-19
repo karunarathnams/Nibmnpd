24956172974b118b0450f6afe4a3db7dfed33d25
karunarathnams <40273899+karunarathnams@users.noreply.github.com>
2018-06-19 17:48:49 +0530

    Add files via upload

---
+++ exercise2.1.c
@@ -0,0 +1,15 @@
+/*exercise-2.1*/
+#include<stdio.h>
+
+int main() 
+
+{
+	printf("University of Moratuwa\n");
+	printf("katubedda,\n");
+	printf("Moratuwa,\n");
+	printf("Sri Lanka\n");
+	printf("---------------------\n");
+	printf("www.mrt.ac.lk\n");
+	return 0;
+
+}
